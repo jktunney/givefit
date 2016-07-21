8537a9eec04b26c3d74fd90130002776842c3bf1
Jake <jktunney5@gmail.com>
2016-07-20 23:43:30 -0400

    added logo and header changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,7 +12,7 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Header.css';
 import Link from '../Link';
 import Navigation from '../Navigation';
-import logoUrl from './logo-small.png';
+import logoUrl from './givefit.png';
 
 function Header() {
   return (
@@ -21,11 +21,11 @@ function Header() {
         <Navigation className={s.nav} />
         <Link className={s.brand} to="/">
           <img src={logoUrl} width="38" height="38" alt="React" />
-          <span className={s.brandTxt}>Your Company</span>
+          <span className={s.brandTxt}>givefit</span>
         </Link>
         <div className={s.banner}>
-          <h1 className={s.bannerTitle}>React</h1>
-          <p className={s.bannerDesc}>Complex web apps made easy</p>
+          <h1 className={s.bannerTitle}>Explore Free Fitness</h1>
+          <p className={s.bannerDesc}>Only in Baltimore</p>
         </div>
       </div>
     </div>
